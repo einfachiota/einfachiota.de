f39ad89e6ce152aa60eeb8c945e54e1a76cae0e4
Cryptec <neupi92@web.de>
2021-02-23 21:44:57 +0100

    changed server.js

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
+const url = dev ? '/' : 'einfachIOTA'; // <<-
+
 polka() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
