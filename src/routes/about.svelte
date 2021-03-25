28206320de07cc665defa06edfcb0db41ce19bf1
IOTABeginnersGuide <80357168+IOTABeginnersGuide@users.noreply.github.com>
2021-03-25 18:21:15 +0100

    Updated Text

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -22,11 +22,11 @@
 <div class="container">
   <h1 class="about">Über uns</h1>
   <br />
-  <p>EinfachIOTA ist von der Community - für die Community! Von Anfang an ist unser Ziel, IOTA für jeden 
-    einfach und verständlich zu machen, und die Community zu unterstützen wo wir nur können. 
+  <p>EinfachIOTA ist von der Community - für die Community! 
+    <br />
+    Wir von einfachIOTA beschäftigen uns mit der Erstellung von deutschsprachigem Content rund um IOTA. Unser Ziel ist es, Interessierten Informationen über IOTA möglichst           einfach und verständlich näher zu bringen und so unseren Teil zum IOTA Ecosystem beizutragen. Die von uns erstellten Inhalte veröffentlichen wir im Blog unserer Homepage.       Eine Übersicht über unsere laufenden Projekte findet ihr auf unserer Startseite. 
   </p>
   <p>
-    Ob mit Software, einem physischen Magazin, oder unserem "Projekt der Woche", 
-    wir begleiten euch wo wir nur können.
+    Bei uns kann jeder mitmachen, der Lust hat, das IOTA Ecosystem voranzubringen. Dabei kann sich jedes Mitglied die Bereiche und den Umfang, in dem es sich einbringen möchte,     frei auswählen. Auch neue Ideen sind herzlich willkommen und können sehr gerne eingebracht und umgesetzt werden. Wenn ihr neugierig geworden seid und gerne bei uns mitmachen     möchtet, meldet euch gerne per Discord, Telegram oder per Mail.
   </p>
   </div>
