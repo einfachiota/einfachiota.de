278ec6245aa524bd7f10c92a409a434ff135052e
Cryptec <neupi92@web.de>
2021-03-30 14:57:49 +0200

    added buttons to pdw

--- src/routes/pdw.svelte
+++ src/routes/pdw.svelte
@@ -11,6 +11,21 @@
     .about {
       text-decoration: underline #00B8C5;
     }
+    .buttons {
+     background-color: #fd6378;
+     border: 1px solid transparent;
+     width: 12em;
+     padding: 15px;
+     margin: 15px;
+     cursor: pointer;
+     font-family: Arial, Helvetica, sans-serif;
+     font-size: large;
+     border-radius: 5px;
+
+    }
+     .buttons:hover {
+      background-color: #e75468  }
+
   
   
   </style>
@@ -26,6 +41,8 @@
       <br />
       Wir von einfachIOTA beschäftigen uns mit der Erstellung von deutschsprachigem Content rund um IOTA. Unser Ziel ist es, Interessierten Informationen über IOTA möglichst           einfach und verständlich näher zu bringen und so unseren Teil zum IOTA Ecosystem beizutragen. Die von uns erstellten Inhalte veröffentlichen wir im Blog unserer Homepage.       Eine Übersicht über unsere laufenden Projekte findet ihr auf unserer Startseite. 
     </p>
+    <a href="https://docs.google.com/document/d/17EKslI2g9swfqJhvkX8xacOs8y5PLya3g_iKnDn_xUw/edit" target="_blank" id="link"><button class="buttons">Deutsch</button></a>
+    <a href="https://docs.google.com/document/d/1jV29k9baofI8AR0sOH5TeNBfRjgI8c8X3khhXSbIPhs/edit" target="_blank" id="link"><button class="buttons">Englisch</button></a>
     
     <br />
     <br />
