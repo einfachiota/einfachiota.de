70350979c49c6de35893e65eb3adb5ba32584b3a
Cryptec <neupi92@web.de>
2021-03-31 16:51:03 +0200

    added dropdown

--- src/routes/nodesoftware.svelte
+++ src/routes/nodesoftware.svelte
@@ -1,127 +1,25 @@
- 
-  <script>
-    export let segment;
-
-    const  TELEGRAM="https://t.me/einfachIOTA", 
-           DISCORD="https://discord.com/invite/eReFR9RzaN";
-  </script>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+  .collapsible {
+    cursor: pointer;
+    padding: 15px;
+    width: 100%;
+    border: none;
+    text-align: left;
+    outline: none;
+    font-size: 15px;
+    overflow: hidden !important;
+  }
   
-  <svelte:head>
-    <title>Nodesoftware</title>
-  </svelte:head>
-  
-  <div class="container">
-    <br />
-    <h1>Ich möchte eine Node aufsetzen!</h1>
-   </div>
-
-   <p>
-    Wer plant eine Node aufzusetzen, der wird erst einmal erschlagen von der fülle an 
-    Anleitungen die es mittlerweile schon gibt - oftmals sind diese bereits nichtmal mehr aktuell.
-    Mit dieser Übersicht wollen wir dir bei der suche nach einer Geeigneten Lösung etwas unter die Arme greifen. 
-   </p>
-   <p class="information">&#x2139;  Solltest du dennoch Hilfe benötigen, oder Fragen haben, die einfachIOTA Community auf 
-    <a href={DISCORD} target="_blank">Discord</a> und
-    <a href={TELEGRAM} target="_blank">Telegram</a>
-      kann dir sicher weiterhelfen!</p>
-   <br />
-
-
-<div class="table-section">
-  <br />
-    <table class="table" id="tblData">
-        <thead>
-          <tr class="tblhead">
-                  <th role="columnheader" style="border-top-left-radius: 4px;">Lösung</th>
-                  <th role="columnheader">Schwierigkeit</th>
-                  <th role="columnheader">Netzwerk</th>
-                  <th role="columnheader">Aktuell / Funktion bestätigt</th>
-                  <th role="columnheader">Sprache</th>
-                  <th role="columnheader" style="border-top-right-radius: 4px;">Unsere Bewertung</th>
-          </tr>
-        </thead>
-        <tbody role="rowgroup">
-          <tr href="https://docs.raspihive.org">
-            <td>Raspihive</td>
-            <td>Einfach</td>
-            <td>Mainnet</td>
-            <td>Ja / 19.03.21</td>
-            <td>EN</td>
-            <td>9/10</td>
-          </tr>
-          <tr>
-            <td>Hornet - Official</td>
-            <td>Moderat</td>
-            <td>Mainnet</td>
-            <td>Ja / 19.03.21</td>
-            <td>EN</td>
-            <td>7/10</td>
-          </tr>
-          <tr>
-            <td>Bee - Official</td>
-            <td>Moderat</td>
-            <td>Mainnet</td>
-            <td>Ja / 19.03.21</td>
-            <td>EN</td>
-            <td>7/10</td>
-          </tr>
-          <tr>
-            <td>Playbook</td>
-            <td>Moderat</td>
-            <td>Mainnet</td>
-            <td>Ja / 19.03.21</td>
-            <td>EN</td>
-            <td>8/10</td>
-          </tr>
-          <tr>
-            <td>GoShimmer - Official</td>
-            <td>Moderat</td>
-            <td>Pollen-Testnet</td>
-            <td>Ja / 19.03.21</td>
-            <td>EN</td>
-            <td>7/10</td>
-          </tr>
-          <tr>
-            <td>GoShimmer+WASP</td>
-            <td>Schwer</td>
-            <td>Pollen-Testnet</td>
-            <td>Ja / 19.03.21</td>
-            <td>EN</td>
-            <td>7/10</td>
-          </tr>
-        </tbody>
-      </table>
-      <br />
-      <br />
-    </div>
-
-<div class="trouble-section">
-      <br />
-      <br />
-      <br />
-      <br />
-      <h3>Funktioniert ein Tutorial nicht mehr?</h3>
-     </div>
-  
-     <p>
-      Wir geben unser bestes, die übersicht Aktuell zu halten. Jedoch besteht unser Team aus freiwilligen,
-      die auch nur begrenzt Zeit haben die verschiedenen Tutorials zu überprüfen. 
-      <br /><br />
-
-      Wenn du einen fehler entdeckst oder etwas nicht mehr funktioniert, gib uns bitte kurz über 
-      <a href={DISCORD} target="_blank">Discord</a> oder
-      <a href={TELEGRAM} target="_blank">Telegram</a>
-      bescheid.
-     </p>
-     <br />
-      <br />
-      <br />
-      <br />
-     <br />
-
-
-
-<style>
+  .content {
+    border: 1px solid transparent;
+    display: none;
+    overflow: hidden !important;
+    background-color: #f1f1f1;
+    padding: 15px;
+    
+  }
 
   .container {
     height: 250px;
@@ -134,7 +32,7 @@
       margin-right: 100px;
       font-family: Arial, Helvetica, sans-serif;
   }
- 
+  
   #tblData {
     font-family: Arial, Helvetica, sans-serif;
     border-collapse: collapse;
@@ -168,34 +66,26 @@
     background-color: rgb(255, 237, 188);
     text-align: center;
   }
-
+  
   #tblData tr:nth-child(even){background-color: rgb(191, 214, 212);}
-
   #tblData tr:hover {background-color: #83e1f7;}
 
-
-  /*
-Max width before this PARTICULAR table gets nasty. This query will take effect for any screen smaller than 760px and also iPads specifically.
-*/
-@media
+  @media
   only screen 
   and (max-width: 760px), (min-device-width: 768px) 
   and (max-device-width: 1024px)  {
-
   /* Force table to not be like tables anymore */
   table, thead, tbody, th, td, tr {
     display: block;
     border-radius: 5px;
     padding: 5px;
   }
-
   /* Hide table headers (but not display: none;, for accessibility) */
   thead tr {
     position: absolute;
     top: -9999px;
     left: -9999px;
   }
-
   tr {
     margin: 0 0 1rem 0;
   }
@@ -212,7 +102,6 @@ Max width before this PARTICULAR table gets nasty. This query will take effect f
     position: relative;
     padding-left: 50%;
   }
-
   td:before {
     /* Now like a table header */
     position: absolute;
@@ -236,7 +125,6 @@ Max width before this PARTICULAR table gets nasty. This query will take effect f
   h1 {
     margin-left: -10px
   }
-
   /*
   Label the data
   You could also use a data-* attribute and content for this. That way "bloats" the HTML, this way means you need to keep HTML and CSS in sync. Lea Verou has a clever way to handle with text-shadow.
@@ -248,4 +136,140 @@ Max width before this PARTICULAR table gets nasty. This query will take effect f
   td:nth-of-type(5):before { content: "Unsere Bewertung:"; }
 }
 
-</style>
+  </style>
+</head>
+
+  <script>
+    export let segment;
+    const  TELEGRAM="https://t.me/einfachIOTA", 
+           DISCORD="https://discord.com/invite/eReFR9RzaN";
+  </script>
+
+  <svelte:head>
+    <title>Nodesoftware</title>
+  </svelte:head>
+  
+<body>
+
+  <div class="container">
+      <br />
+      <h1>Ich möchte eine Node aufsetzen!</h1>
+  </div>
+
+  <p>
+    Wer plant eine Node aufzusetzen, der wird erst einmal erschlagen von der fülle an 
+    Anleitungen die es mittlerweile schon gibt - oftmals sind diese bereits nichtmal mehr aktuell.
+    Mit dieser Übersicht wollen wir dir bei der suche nach einer Geeigneten Lösung etwas unter die Arme greifen. 
+   </p>
+   <p class="information">&#x2139;  Solltest du dennoch Hilfe benötigen, oder Fragen haben, die einfachIOTA Community auf 
+    <a href={DISCORD} target="_blank">Discord</a> und
+    <a href={TELEGRAM} target="_blank">Telegram</a>
+      kann dir sicher weiterhelfen!</p>
+   <br />
+
+
+   <div class="table-section">
+    <br />
+      <table class="table" id="tblData">
+          <thead>
+            <tr class="tblhead">
+                    <th role="columnheader" style="border-top-left-radius: 4px;">Lösung</th>
+                    <th role="columnheader">Schwierigkeit</th>
+                    <th role="columnheader">Netzwerk</th>
+                    <th role="columnheader">Aktuell / Funktion bestätigt</th>
+                    <th role="columnheader">Sprache</th>
+                    <th role="columnheader" style="border-top-right-radius: 4px;">Unsere Bewertung</th>
+            </tr>
+          </thead>
+
+          <tbody role="rowgroup">
+            <tr type="button" class="collapsible">
+              <td>Raspihive</td>
+              <td>Einfach</td>
+              <td>Mainnet</td>
+              <td>Ja / 19.03.21</td>
+              <td>EN</td>
+              <td>9/10</td>
+            </tr>
+            <a class="content">
+              <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+            </a>
+            <tr type="button" class="collapsible">
+              <td>Hornet - Official</td>
+              <td>Moderat</td>
+              <td>Mainnet</td>
+              <td>Ja / 19.03.21</td>
+              <td>EN</td>
+              <td>7/10</td>
+            </tr>
+            <a class="content">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </a>
+          </tbody>
+
+        </table>
+        <br />
+        <br />
+      </div>
+
+
+  <button type="button" class="collapsible">Open Section 1</button>
+  <a class="content">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  </a>
+  <button type="button" class="collapsible">Open Section 2</button>
+  <div class="content">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  </div>
+  <button type="button" class="collapsible">Open Section 3</button>
+  <div class="content">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  </div>
+  
+
+  <div class="trouble-section">
+    <br />
+    <br />
+    <br />
+    <br />
+    <h3>Funktioniert ein Tutorial nicht mehr?</h3>
+   </div>
+
+   <p>
+    Wir geben unser bestes, die übersicht Aktuell zu halten. Jedoch besteht unser Team aus freiwilligen,
+    die auch nur begrenzt Zeit haben die verschiedenen Tutorials zu überprüfen. 
+    <br /><br />
+
+    Wenn du einen fehler entdeckst oder etwas nicht mehr funktioniert, gib uns bitte kurz über 
+    <a href={DISCORD} target="_blank">Discord</a> oder
+    <a href={TELEGRAM} target="_blank">Telegram</a>
+    bescheid.
+   </p>
+   <br />
+    <br />
+    <br />
+    <br />
+   <br />
+
+
+
+
+  <script>
+    
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+  
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
+  </script>
+  
+</body>
